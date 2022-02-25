519750cb7b56909bf812331cca357bef817c2ce9
yuukiiwa <ywan6@alumni.jh.edu>
2022-02-25 13:56:18 +0800

    change separator

--- proactiv-aggregator/proActiv_aggregator.r
+++ proactiv-aggregator/proActiv_aggregator.r
@@ -23,7 +23,7 @@
 ################################################
 args = commandArgs(trailingOnly=TRUE)
 
-dir     <- strsplit(grep('--condition*', args, value = TRUE), split = '=')[[1]][[2]]
+dir     <- strsplit(grep('--dir*', args, value = TRUE), split = '=')[[1]][[2]]
 
 ################################################
 ################################################
@@ -34,12 +34,14 @@ csv_list <- list.files(dir, pattern= "*_proActiv_count.csv")
 ##output the following merged matricies for downstream analysis
 outfns <-c("transcript_mean_merged.csv","gene_mean_merged.csv",
            "promoter_class_merged.csv","absolute_activity_merged.csv")
+print(csv_list)
 for (i in 1:length(outfns)){
     for (j in 1:length(csv_list)){
         df <- read.table(file.path(dir,csv_list[j]), header=TRUE, sep=",")
         if (j == 1){
             original_colnames <- colnames(df)
             index <- length(original_colnames)-4
+            print(original_colnames)
             original_colnames <- original_colnames[1:index]
             merged_df <- df[,1:index]
             merged_df <- data.frame(merged_df,df[index+1])
